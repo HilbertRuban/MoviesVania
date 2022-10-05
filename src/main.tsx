c7fc32e4a5c1e6329c3660b92fe0d5c4a8e90c4b
Hilbert <hilbertruban53@gmail.com>
2022-10-05 15:45:07 +0530

    improved cs

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./index.css";
 
 const theme = createTheme({});
